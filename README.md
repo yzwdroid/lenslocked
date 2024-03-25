cd697c783108469d04ade76ae87d0faf21772436
Andy Yang <yzwdroid@gmail.com>
2024-03-25 13:03:47 -0400

    templates

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Learn to build real, production grade web applications.
 
-- go modules
+## go modules
 
 ```go
 go mod init github.com/yzwdroid/lenslocked
@@ -10,7 +10,7 @@ go install
 lenslocked
 ```
 
-- Dynamic Reloading
+## Dynamic Reloading
 
 Dynamic reloading is basically the act of watching for changes in your code, and then automatically rebuilding and restarting your program once a change is detected.
 ```
@@ -25,5 +25,14 @@ go install github.com/cortesi/modd/cmd/modd@latest
   prep: go build -o lenslocked .
   daemon +sigterm: ./lenslocked
 }
-
 ```
+
+- Setting Header Values `Content-Type`
+
+common ones: `text/html, application/json, image/png, video/mp4`
+
+## Router
+```
+go get -u github.com/go-chi/chi/v5
+```
+The -u flag instructs get to update modules providing dependencies of packages named on the command line to use newer minor or patch releases when available.
--- main.go
+++ main.go
@@ -2,15 +2,52 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
+
+	"github.com/go-chi/chi/v5"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+func executeTemplate(w http.ResponseWriter, filepath string) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	tpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		log.Printf("processing template: %v", err)
+		http.Error(w, "There was an error processing the template.", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tplPath)
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tplPath)
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 }
